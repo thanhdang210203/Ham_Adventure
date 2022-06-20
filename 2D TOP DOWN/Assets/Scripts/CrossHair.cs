cf799b637c530401aec07ab533c9b66fba60826d
thanhdang210203 <thanhdang210203@gmail.com>
2022-06-20 17:17:19 +0700

    smth

--- 2D TOP DOWN/Assets/Scripts/CrossHair.cs
+++ 2D TOP DOWN/Assets/Scripts/CrossHair.cs
@@ -14,4 +14,109 @@ public class CrossHair : MonoBehaviour
     public float runJumpSize = 125f;
     public float currentSize = 50f;
     public float speed = 10f;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+    private void Update()
+    {
+        if (Aiming)
+        {
+            currentSize = Mathf.Lerp(currentSize, aimSize, Time.deltaTime * speed);
+        }
+        else if (Walking)
+        {
+            currentSize = Mathf.Lerp(currentSize, walkSize, Time.deltaTime * speed);
+        }
+        else if (Running)
+        {
+            currentSize = Mathf.Lerp(currentSize, runJumpSize, Time.deltaTime * speed);
+        }
+        else
+        {
+            currentSize = Mathf.Lerp(currentSize, idleSize, Time.deltaTime * speed);
+        }
+
+        crossHair.sizeDelta = new Vector2(currentSize, currentSize);
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+    bool Walking
+    {
+        get
+        {
+            if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                if(Input.GetKey(KeyCode.LeftShift) == false)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    bool Running
+    {
+        get
+        {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                if (Input.GetKey(KeyCode.LeftShift) == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public bool Aiming
+    {
+        get
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                if(!Walking && !Running)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    //private void OnMouseEnter()
+    //{
+    //    Cursor.SetCursor(null, hotSpot, cursorMode);
+    //    mousePosition = Input.mousePosition;
+    //    mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    //    transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    //}
+
+    //void OnMouseExit()
+    //{
+    //    Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    //}
 }
--- 2D TOP DOWN/Assets/Scripts/Mouse.cs
+++ 2D TOP DOWN/Assets/Scripts/Mouse.cs
@@ -8,15 +8,40 @@ public class Mouse : MonoBehaviour
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
-
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
     private void OnMouseEnter()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        Cursor.SetCursor(null, hotSpot, cursorMode);
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
     }
 
     void OnMouseExit()
     {
         Cursor.SetCursor(null, Vector2.zero, cursorMode);
     }
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+
+    }
 }
+
 
