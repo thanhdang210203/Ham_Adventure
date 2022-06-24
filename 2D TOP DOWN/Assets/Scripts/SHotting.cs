cd9b0e559642c9f5c547099a011405db6960865d
thanhdang210203 <thanhdang210203@gmail.com>
2022-06-24 12:51:32 +0700

    f

--- 2D TOP DOWN/Assets/Scripts/SHotting.cs
+++ 2D TOP DOWN/Assets/Scripts/SHotting.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SHotting : MonoBehaviour
@@ -11,23 +12,29 @@ public class SHotting : MonoBehaviour
     public AudioClip reload;
     public AudioClip bulletShoot;
     public AudioClip emty;
+    public GameObject gun1;
+    public GameObject gun2;
+    public GameObject gun3;
+    public AudioClip pickUp;
 
     #endregion Public var
 
     #region Private var
 
-    [SerializeField] private int maxBulletPerMag = 20;
     [SerializeField] private int currentBullet;
     [SerializeField] private bool ableToShoot = true;
     [SerializeField] private int lowAmmo;
     [SerializeField] private bool bulletIsLow = false;
     [SerializeField] private bool ableToReload = false;
+    [SerializeField] private bool gun1Pickup = false;
 
     #endregion Private var
 
     private void Start()
     {
-        currentBullet = maxBulletPerMag;
+        currentBullet = 0;
+        totalBullet = 0;
+        ableToReload = false;
     }
 
     // Update is called once per frame
@@ -54,33 +61,52 @@ public class SHotting : MonoBehaviour
                 AudioSource.PlayClipAtPoint(emty, transform.position);
             }
         }
-        else if (Input.GetKey(KeyCode.R))
+        if (Input.GetKey(KeyCode.R))
         {
             if (ableToReload)
             {
-                Reload();
+                StartCoroutine(Reload());
+            }
+            else if (!ableToReload && ableToShoot)
+            {
+                //AudioSource.PlayClipAtPoint(emty, transform.position);
+                //Audio plays multiple times at once -> maybe display text
+                Debug.Log("Still got some bullet there mate");
             }
             else if (!ableToReload)
             {
-                AudioSource.PlayClipAtPoint(emty, transform.position);
+                Debug.Log("Fuck u");
             }
         }
     }
 
     private void Shoot()
     {
-        currentBullet -= 1;
+        currentBullet--;
         AudioSource.PlayClipAtPoint(bulletShoot, transform.position);
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D Player = bullet.GetComponent<Rigidbody2D>();
         Player.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
     }
 
-    private void Reload()
+    private IEnumerator Reload()
     {
-        AudioSource.PlayClipAtPoint(reload, transform.position);
+        AudioSource.PlayClipAtPoint(reload, this.transform.position);
+        yield return new WaitForSeconds(reload.length);
         currentBullet += 20;
         totalBullet -= 20;
         Debug.Log("Reloading");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gun1)
+        {
+            gun1Pickup = true;
+            AudioSource.PlayClipAtPoint(pickUp, gun1.transform.position);
+            currentBullet += 20;
+            totalBullet += 100;
+            Destroy(gun1);
+        }
     }
 }
